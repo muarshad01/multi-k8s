0535c531306d4fea7cb93e6a7e077b9f3020bdb8
Muhammad Arshad <marshad@marshad-ltm4voy.internal.salesforce.com>
2022-08-06 22:34:31 -0400

    updated files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t cygnetops/multi-client-k8s:latest       -t cygnetops/multi-client-k8s:$SHA       -f ./client/Dockerfile ./client
 docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t cygnetops/multi-worker-k8s:latest       -t cygnetops/multi-worker-k8s:$SHA       -f ./worker/Dockerfile ./worker
 
 docker push cygnetops/multi-client-k8s:latest
 docker push cygnetops/multi-server-k8s-pgfix:latest
